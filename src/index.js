a5bf22a09ce0f9b177e959e675dda77898a233d8
Court Schuett <schuettc@amazon.com>
2022-11-16 17:56:28 -0600

    fix: downgrade to 16

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme } from 'amazon-chime-sdk-component-library-react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
     <ThemeProvider theme={lightTheme}>
       <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    </ThemeProvider>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
